c7fa26e52d5103773e27e672115f4b2f9392c452
nahiumeler <nahiumel04@gmail.com>
2024-11-05 22:35:49 -0300

    Add files via upload

---
+++ Vue JS/ejemplo1-Vue/js/app.js
@@ -0,0 +1,11 @@
+let app = Vue.createApp({
+    data: function(){
+        return {
+            mensaje: 'Hola Mundo',
+            saludar: 'Hola amigo'
+        }
+    }
+})
+app.mount('#app')
+
+// esta el mismo codigo que escribi en html 
